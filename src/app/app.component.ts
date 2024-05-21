ebf0d2bca0b860b4ba7d7882fe8e2ef84a43bc01
victorbdev <victor.balboa.dev@gmail.com>
2024-05-21 00:04:36 -0500

    initial commit :fire:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'dawa_sem11';
+  nombre = '';
 }
+
+// export class AppComponent {
+//   onClick() {
+//     alert('Utilizando eventos en Angular')
+//   }
+// }
+
+// export class AppComponent {
+//   title = 'Semana 11';
+//   valor = "Víctor Balboa Mercado"
+//   //tipo = "hidden"
+//   tipo = "password"
+//   img = "assets/comercio.png"
+//   enlace = "https://elcomercio.pe/"
+//   clase01 = "azul"
+//   clase02 = "rojo"
+// }
